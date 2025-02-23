667640f1b4ea98ad60a98e80c1e78b338343499a
Seidkhan <seidkhan93@mail.ru>
2025-02-23 22:03:30 +0500

    change in heading in popular movies

--- pages/popular.vue
+++ pages/popular.vue
@@ -3,7 +3,7 @@
     <section>
       <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
       >
-        Popular Movies
+        Popular Movies (Films)
       </h1>
 
       <ul class="grid grid-cols-5 gap">
